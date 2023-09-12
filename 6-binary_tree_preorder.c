dc3e129f06154004d195ec72eb99d892041b6f34
carlfrank <carl-frank7@hotmail.com>
2023-09-12 10:02:53 -0400

    preorder check again

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,12 +5,12 @@
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node.
  */
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
-
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
 }
