01ba019e571ba8a132e888b890fd5ee5ce103925
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-09-13 20:19:14 +0000

    fix according to betty-style: add parenthesis to return values

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,16 +19,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new_node->left = (NULL);
+	new_node->right = (NULL);
 
 	if (parent->right != NULL)
 	{
@@ -38,5 +38,5 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	parent->right = new_node;
 
-	return new_node;
+	return (new_node);
 }
