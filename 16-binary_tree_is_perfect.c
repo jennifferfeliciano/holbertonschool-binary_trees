910dec66b05b3a5369d690107ee9c34887aa4271
jenniffer <jenniffer.felicianovelez@gmail.com>
2023-09-14 03:29:47 +0000

    Write a function that checks if a binary tree is perfectwq

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,24 +1,42 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * binary_tree_is_perfect- function that checks if a binary tree is perfect
  * @tree: is a pointer to the root node
  * Return: 1 if perfect 0 is not
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return 0;
+	int depth;
 
-    int left_height = binary_tree_height(tree->left);
-    int right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+		return (0);
 
-    if (left_height == right_height) {
-        if (tree->left == NULL && tree->right == NULL)
-            return 1;
-        else if (tree->left != NULL && tree->right != NULL)
-            return binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right);
-    }
+	depth = -1;
+	return (is_perfect_recursive(tree, 0, &depth));
+}
 
-    return 0;
+/**
+ * perfect_recursive- helper function to check if a binary tree is perfect
+ * @node: current node being checked
+ * @current_depth: current depth in the tree
+ * @depth: pointer to store the depth of the first NULL node
+ * Return: 1 if perfect, 0 if not
+ */
+
+int perfect_recursive(const binary_tree_t *node, int current_depth, int *depth)
+{
+	if (node == NULL)
+	{
+		if (*depth == -1)
+		{
+			*depth = current_depth;
+			return (1);
+		}
+		return (current_depth == *depth);
+	}
+
+	return (is_perfect_recursive(node->left, current_depth + 1, depth) &&
+			is_perfect_recursive(node->right, current_depth + 1, depth));
 }
